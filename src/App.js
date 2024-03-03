417048a559c8f567a4673a5017578e761daf48b2
norris-mos <maxonorris@gmail.com>
2024-03-03 14:34:49 +0000

    adding the teacher student page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
-
 import Home from './components/Home/Home';
+import teacherImage from './assets/broonie_teacher.png';
+import studentImage from './assets/broonie_student.png';
 
 function App() {
+  const [isLandingPageVisible, setIsLandingPageVisible] = useState(true);
+
+  const navigateToHome = () => {
+    setIsLandingPageVisible(false);
+  };
+
   return (
     <div className="App">
-      <Home />
+      {isLandingPageVisible ? (
+        <div className="landing-page">
+          <h1>broonie.ai</h1> {/* Updated text */}
+          <div className="image-panel">
+            <div onClick={navigateToHome} className="image-container teacher">
+              <img src={teacherImage} alt="Teacher" />
+              <p>Teacher</p>
+            </div>
+            <div onClick={navigateToHome} className="image-container student">
+              <img src={studentImage} alt="Student" />
+              <p>Student</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,34 @@
 // Card.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css'; // Make sure to create this CSS file and include the styles
+import Modal from './Modal/Modal';
 
 const Card = ({ title, description, icon, isNew }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
-    <div className={`card ${isNew ? 'new' : ''}`}>
-      <div className="card-header">
-        <span className="card-icon">{icon}</span>
-        {isNew && <span className="badge">NEW!</span>}
+    <>
+      <div className={`card ${isNew ? 'new' : ''}`} onClick={toggleModal}>
+        <div className="card-header">
+          <span className="card-icon">{icon}</span>
+          {isNew && <span className="badge">NEW!</span>}
+        </div>
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <p className="card-description">{description}</p>
+        </div>
       </div>
-      <div className="card-body">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>
-      </div>
-    </div>
+      <Modal isOpen={isModalOpen} closeModal={toggleModal}>
+        {/* Content you want to display in the modal goes here */}
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </Modal>
+    </>
   );
 };
 
